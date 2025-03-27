cda59d9f490ce7cf4daf971c91ef87a61e645bb6
Tirth02 <pateltirth0902@gmail.com>
2025-03-27 12:13:30 +0530

    function,function call, use-client done

--- blog/src/app/page.js
+++ blog/src/app/page.js
@@ -1,15 +1,41 @@
-import Image from "next/image";
+'use client'
+import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  // states in NextJS (when the state is change page re renders)
+  const [name,setName] = useState("Tirth");
+
+  // whereas in variable page is not re render when it changes
+  let name2 = "Tirth";
+  // function in next 
+  const apple = () =>{
+    //alert("fRUIT");
+    // setName("Manish");
+    name2 = "Manish";
+  }
+
+  const InnerComp = () =>{
+    return (
+      <h1>Inner Component</h1>
+    )
+  }
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Home Page</h1>
-        <User name="Tirth"/>
-        <User name="Jaswant"/>
-        <User name="Yash"/>
-        <User name="Het"/>
+        {/* <h2>This is {name} from Ahmedabad</h2> */}
+        <h2>This is {name2} from Ahmedabad</h2>
+        
+
+        {/* below both thingperforms same thing what is the difference we will see it in future */}
+        {/* <InnerComp/> */}
+        {InnerComp()}
+
+        {/*  function call in nextJS */}
+        <button onClick={() => apple()}>Click me</button>
+
       </main>
       
     </div>
