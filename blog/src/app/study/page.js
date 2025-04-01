92a55a7de8443e884b7394944a7d52d5c9577814
Tirth02 <pateltirth0902@gmail.com>
2025-04-01 15:31:40 +0530

    conditional style and CSS explained

--- blog/src/app/study/page.js
+++ blog/src/app/study/page.js
@@ -1,9 +1,17 @@
-import React from 'react'
+"use client"
+import custom from '../custom.module.css';
+import React, { useState } from 'react'
 
 const Study = () => {
+  const [color,setColor] = useState("red");
+  const {red} = custom;
   return (
     <div>
       <h1>All Lectures of College</h1>
+      <h1 className={color=='red'?custom.red:custom.green}>Condition with style</h1>
+      <h2 id={custom.purple}>Heading 2</h2>
+
+      <h3 className={red}>Dummy text</h3>
     </div>
   )
 }
