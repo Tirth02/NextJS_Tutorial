ccfaf2168694e44065a5bac388208e43af776b9f
Tirth02 <pateltirth0902@gmail.com>
2025-04-03 23:25:10 +0530

    Next Font explained

---
+++ blog/src/app/random/nextfont/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+    weight:"100",
+    subsets:['latin'],
+    display: 'swap'
+})
+const NextFont = () => {
+  return (
+    <div>
+      {/* Font Optimization helps in preventing unecessary call to google fonts ie reduce network request and also security is maintained */}
+      <h1 className={roboto.className}>Font Optimization in Next</h1>
+    </div>
+  )
+}
+
+export default NextFont
